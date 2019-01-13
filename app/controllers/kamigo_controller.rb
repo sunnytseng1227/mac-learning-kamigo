e385dc96540feff5a2677752244776c41ae7fbcd
sunnytseng <sunny.ann.tseng@gmail.com>
2019-01-13 23:34:41 +0800

    no message

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -15,7 +15,10 @@ class KamigoController < ApplicationController
 
     body = request.body.read
 
-    
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
 
     events = line.parse_events_from(body)
 
